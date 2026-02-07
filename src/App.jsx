6944129aa9d24b56b54124ed602be41af3aa57ad
Patrick Behr (global) <patrick@global.com>
2026-02-07 12:08:01 -0800

    Hello, World

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import Region from './Region.jsx'
 
-function App( ) {
+function App({origin="./data"}) {
   return (
     <div className="app">
-        <Region 
-            region="Canada"
-            url="./data/Canada.json"
-        />
+        <Region region="America"        url={`${origin}/America.json`} />
+        <Region region="Canada"         url={`${origin}/Canada.json`} />
+        <Region region="Australia"      url={`${origin}/Australia.json`} />
+        <Region region="Europe"         url={`${origin}/Europe.json`} />
+        <Region region="South America"  url={`${origin}/South_America.json`} />
     </div>
   )
 }
